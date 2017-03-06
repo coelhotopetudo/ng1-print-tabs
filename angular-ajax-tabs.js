f7eb050beaac3217fdfa0e1b23caeb81dd0072d2
Alisson Coelho de Morais <coelhotopetudo@gmail.com>
2017-03-06 15:09:18 -0300

    lendo parametros get

---
+++ angular-ajax-tabs.js
@@ -0,0 +1,87 @@
+(function(window, angular, undefined) {'use strict';
+
+var ngAJAXTabs = angular.module("ngAJAXTabs", []);
+
+ngAJAXTabs.directive("tabs", function() {
+    return {
+		restrict: "E",
+		transclude: true,
+		scope: {},
+		controller: function($scope, $element) {
+			var panes = $scope.panes = [];
+
+        $scope.select = function(pane) {
+            angular.forEach(panes, function(rane, pane) {
+                rane.selected = false;
+            });
+            if (pane.load !== undefined) {
+                pane.load();
+            }
+            pane.selected = true;
+
+            if (pane.visivel) {
+                var ranes = [];
+                ranes = angular.element(panes);
+                ranes.remove(pane);
+            }
+            pane.visivel = !pane.visivel;
+        };
+
+      this.addPane = function(pane) {
+				if (panes.length === 0) $scope.select(pane);
+				panes.push(pane);
+			};
+
+			  this.delPane = function(pane) {
+            panes = panes.filter(function(el){return el !== pane;});
+			  };
+		},
+		template:
+		  '<div class="tabbable">' +
+		    '<ul class="nav nav-tabs">' +
+		      '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
+		        '<a href="" ng-click="select(pane)">{{pane.tabTitle}}</a>' +
+		      '</li>' +
+		    '</ul>' +
+		    '<div class="tab-content" ng-transclude></div>' +
+		  '</div>',
+		replace: true
+    };
+});
+
+ngAJAXTabs.directive("pane", ["$http", "$templateCache", "$controller", "$compile", function($http, $templateCache, $controller, $compile) {
+	return {
+		require: "^tabs",
+		restrict: "E",
+		transclude: true,
+		  scope: {
+          tabTitle: "@"
+      },
+		link: function(scope, element, attrs, tabsCtrl) {
+			var templateCtrl, templateScope;
+
+			if (attrs.template && attrs.controller) {
+				scope.load = function() {
+					$http.get(attrs.template, {cache: $templateCache})
+					.then(function(response) {
+						templateScope = scope.$new();
+						templateScope.isTabbedPane = true;
+						templateCtrl = $controller(attrs.controller, {$scope: templateScope});
+              templateScope.visivel = !templateScope.visivel;
+						element.html(response.data);
+						element.children().data('$ngControllerController', templateCtrl);
+						$compile(element.contents())(templateScope);
+					});
+				};
+			}
+
+			tabsCtrl.addPane(scope);
+		},
+		template:
+	        '<div class="tab-pane" ng-class="{active: selected}" ng-transclude>' +
+	        '</div>',
+		replace: false
+	};
+}]);
+
+})(window, window.angular);
---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var AJAXTabsDemo = angular.module("AJAXTabsDemo", [
+	"ngRoute",
+	"ngAJAXTabs",
+	"controllers"
+]).config(["$routeProvider", "$locationProvider",
+	function($routeProvider, $locationProvider) {
+		$routeProvider.
+			when("/pane-1", {
+				controller: "Pane1",
+				templateUrl: "pane-1.html"
+			}).
+			when("/pane-2", {
+				controller: "Pane2",
+				templateUrl: "pane-2.html"
+			}).
+			when("/pane-3", {
+				controller: "Pane3",
+				templateUrl: "pane-3.html"
+			}).
+			otherwise({
+				redirectTo:"/pane-1"
+			});
+	}
+          ]);
+
+var controllers = angular.module("controllers", []).run(function($rootScope, $routeParams) {
+    $rootScope.panes = [
+        { "vis": false, "name": "Pane 1", "path":"pane-1", "partial":"pane-1.html", "controller":"Pane1", "includedInTabView":true },
+        { "vis": true, "name": "Pane 2", "path":"pane-2", "partial":"pane-2.html", "controller":"Pane2", "includedInTabView":true },
+        { "vis": false, "name": "Pane 3", "path":"pane-3", "partial":"pane-3.html", "controller":"Pane3", "includedInTabView":false }
+    ];
+}).controller("Pane1", function($scope) {
+    $scope.textFromControllerScope = "Roo";
+}).controller("Pane2", function($scope) {
+    $scope.textFromControllerScope = "Bar";
+}).controller("Pane3", function($scope, $routeParams) {
+    /* Pane3 controller functionality */
+    $scope.dois = $routeParams;
+})
